2f1ffc6f179550cd52ed0b5d05748c3ac4561128
Soham Srimany <73331793+soham0511@users.noreply.github.com>
2021-06-21 15:37:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAuPqFDw_lnNu43hDz9z1E5eBacTkZRbiI",
-    authDomain: "linkedin-clone-15974.firebaseapp.com",
-    projectId: "linkedin-clone-15974",
-    storageBucket: "linkedin-clone-15974.appspot.com",
-    messagingSenderId: "699369230611",
-    appId: "1:699369230611:web:a9926b4acd7b1edb470ae6",
-    measurementId: "G-WW24FHYWM9"
+    apiKey: "X",
+    authDomain: "X",
+    projectId: "X",
+    storageBucket: "X",
+    messagingSenderId: "X",
+    appId: "X:X:X:X",
+    measurementId: "X"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
